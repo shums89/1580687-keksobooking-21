4f44b5c0258d86d52a38c84ecd5f2d04eb48ef53
Александр <sh889@ya.ru>
2020-10-02 01:28:14 +0500

    Добавил карточку обьявления

--- js/main.js
+++ js/main.js
@@ -80,8 +80,7 @@ function createAdFragment(ad, adsTemplate, adsData = ADS_DATA) {
   return adElement;
 }
 
-function getAdsFragment() {
-  const ads = getAds();
+function getAdsFragment(ads) {
   const adsTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
   const newFragment = document.createDocumentFragment();
 
@@ -92,14 +91,106 @@ function getAdsFragment() {
   return newFragment;
 }
 
+// Функция для конвертации англоязычного типа жилья в русскоязычный
+function convertTypeHouse(typeEng) {
+  let typeRus = ``;
+  switch (typeEng) {
+    case `palace`:
+      typeRus = `Дворец`;
+      break;
+    case `flat`:
+      typeRus = `Квартира`;
+      break;
+    case `house`:
+      typeRus = `Дом`;
+      break;
+    case `bungalow`:
+      typeRus = `Бунгало`;
+  }
+  return typeRus;
+}
+
+function createCardAdPhotoFragment(ad, cardAdElementPhoto) {
+  const cardAdElementPhotoTemplate = cardAdElementPhoto;
+  const newFragmentPhoto = document.createDocumentFragment();
+
+  cardAdElementPhotoTemplate.src = ad.offer.photos[0];
+  for (let i = 1; i < ad.offer.photos.length; i++) {
+    let newCardAdElementPhoto = cardAdElementPhotoTemplate.cloneNode(true);
+    newCardAdElementPhoto.src = ad.offer.photos[i];
+    newFragmentPhoto.appendChild(newCardAdElementPhoto);
+  }
+
+  return newFragmentPhoto;
+}
+
+function createCardAdFragment(ad, cardAdsTemplate) {
+  const cardAdElement = cardAdsTemplate.cloneNode(true);
+
+  cardAdElement.querySelector(`.popup__avatar`).src = ad.author.avatar;
+  cardAdElement.querySelector(`.popup__title`).textContent = ad.offer.title;
+  cardAdElement.querySelector(`.popup__text--address`).textContent = ad.offer.address;
+  cardAdElement.querySelector(`.popup__text--price`).textContent = `${ad.offer.price}₽/ночь`;
+  cardAdElement.querySelector(`.popup__type`).textContent = convertTypeHouse(ad.offer.type);
+  cardAdElement.querySelector(`.popup__text--capacity`).textContent = `${ad.offer.rooms} комнаты для ${ad.offer.guests} гостей`;
+  cardAdElement.querySelector(`.popup__text--time`).textContent = `Заезд после ${ad.offer.checkin}, выезд до ${ad.offer.checkout}`;
+  cardAdElement.querySelector(`.popup__description`).textContent = ad.offer.description;
+
+  let cardAdElementChildren = cardAdElement.querySelector(`.popup__features`).querySelectorAll(`.popup__feature`);
+
+  let arrFeatures = ad.offer.features;
+  for (let i = cardAdElementChildren.length - 1; i >= 0; i--) {
+    const child = cardAdElementChildren[i];
+    let isDelete = true;
+
+    for (let j = 0; j < arrFeatures.length; j++) {
+      if (child.getAttribute(`class`).indexOf(`popup__feature--${arrFeatures[j]}`) > 0) {
+        arrFeatures.splice(j, 1);
+        isDelete = false;
+        break;
+      }
+    }
+
+    if (isDelete) {
+      child.remove();
+    }
+  }
+
+  cardAdElementChildren = cardAdElement.querySelector(`.popup__photos`);
+  const cardAdElementPhoto = cardAdElementChildren.querySelector(`.popup__photo`);
+
+  if (!ad.offer.photos.length) {
+    cardAdElementPhoto.remove();
+  } else if (ad.offer.photos.length === 1) {
+    cardAdElementPhoto.src = ad.offer.photos[0];
+  } else {
+    cardAdElementChildren.appendChild(createCardAdPhotoFragment(ad, cardAdElementPhoto));
+  }
+
+  return cardAdElement;
+}
+
+function getCardAdFragment(ad) {
+  const cardAdsTemplate = document.querySelector(`#card`).content.querySelector(`.map__card`);
+  const newFragment = document.createDocumentFragment();
+
+  newFragment.appendChild(createCardAdFragment(ad, cardAdsTemplate));
+  return newFragment;
+}
+
 function main() {
   const map = document.querySelector(`.map`);
   map.classList.remove(`map--faded`);
 
-  const adsListElement = map.querySelector(`.map__pins`);
-  const adsFragment = getAdsFragment();
+  const ads = getAds();
 
+  const adsListElement = map.querySelector(`.map__pins`);
+
+  const adsFragment = getAdsFragment(ads);
   adsListElement.appendChild(adsFragment);
+
+  const cadrAdFragment = getCardAdFragment(ads[0]);
+  adsListElement.after(cadrAdFragment);
 }
 
 main();
